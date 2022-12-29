cd16c4b90603b39fc588d41c75df8a2b6e39892d
Tất Trung <buitrungt@gmail.com>
2022-12-29 16:05:31 +0700

    init db migration

---
+++ prisma/migrations/20221229090223_init/migration.sql
@@ -0,0 +1,163 @@
+-- CreateTable
+CREATE TABLE "banners" (
+    "id" BIGSERIAL NOT NULL,
+    "image_url" VARCHAR(255),
+    "is_active" BOOLEAN NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "type" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "banners_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" BIGSERIAL NOT NULL,
+    "description" TEXT,
+    "is_author" BOOLEAN NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "slug" VARCHAR(255) NOT NULL,
+    "created_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    "parent_category_id" BIGINT,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "deliveries" (
+    "id" BIGSERIAL NOT NULL,
+    "index" VARCHAR(255) NOT NULL,
+    "value" VARCHAR(255) NOT NULL,
+    "created_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "deliveries_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "order_items" (
+    "id" BIGSERIAL NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "product_id" BIGINT NOT NULL,
+    "sale_order_id" BIGINT NOT NULL,
+    "created_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "order_items_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "product_images" (
+    "id" BIGSERIAL NOT NULL,
+    "image_public_id" VARCHAR(255),
+    "image_url" VARCHAR(255) NOT NULL,
+    "product_id" BIGINT NOT NULL,
+    "created_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "product_images_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "product_rates" (
+    "id" BIGSERIAL NOT NULL,
+    "comment" TEXT,
+    "value" INTEGER NOT NULL,
+    "product_id" BIGINT NOT NULL,
+    "user_id" BIGINT NOT NULL,
+    "created_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "product_rates_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "products" (
+    "id" BIGSERIAL NOT NULL,
+    "author" VARCHAR(255),
+    "current_number" INTEGER NOT NULL,
+    "long_description" TEXT NOT NULL,
+    "number_of_page" INTEGER NOT NULL,
+    "price" BIGINT NOT NULL,
+    "quantity_purchased" INTEGER,
+    "short_description" VARCHAR(255),
+    "slug" VARCHAR(255) NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "category_id" BIGINT NOT NULL,
+    "created_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "sale_orders" (
+    "id" BIGSERIAL NOT NULL,
+    "customer_address" VARCHAR(255) NOT NULL,
+    "phone" VARCHAR(255) NOT NULL,
+    "delivery_id" BIGINT NOT NULL,
+    "user_id" BIGINT NOT NULL,
+    "created_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "sale_orders_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" BIGSERIAL NOT NULL,
+    "address" VARCHAR(255) NOT NULL,
+    "amount" BIGINT NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "first_name" VARCHAR(255) NOT NULL,
+    "last_name" VARCHAR(255) NOT NULL,
+    "username" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "phone" VARCHAR(255) NOT NULL,
+    "role" VARCHAR(255) NOT NULL,
+    "created_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "deliveries_index_key" ON "deliveries"("index");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "order_items_product_id_sale_order_id_key" ON "order_items"("product_id", "sale_order_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "product_rates_user_id_product_id_key" ON "product_rates"("user_id", "product_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- AddForeignKey
+ALTER TABLE "categories" ADD CONSTRAINT "categories_parent_category_id_fkey" FOREIGN KEY ("parent_category_id") REFERENCES "categories"("id") ON DELETE NO ACTION ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "order_items" ADD CONSTRAINT "order_items_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE NO ACTION ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "order_items" ADD CONSTRAINT "order_items_sale_order_id_fkey" FOREIGN KEY ("sale_order_id") REFERENCES "sale_orders"("id") ON DELETE NO ACTION ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "product_images" ADD CONSTRAINT "product_images_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE NO ACTION ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "product_rates" ADD CONSTRAINT "product_rates_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE NO ACTION ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "product_rates" ADD CONSTRAINT "product_rates_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "products" ADD CONSTRAINT "products_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE NO ACTION ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "sale_orders" ADD CONSTRAINT "sale_orders_delivery_id_fkey" FOREIGN KEY ("delivery_id") REFERENCES "deliveries"("id") ON DELETE NO ACTION ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "sale_orders" ADD CONSTRAINT "sale_orders_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE CASCADE;
